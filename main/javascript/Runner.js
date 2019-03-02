2ffe888f0ee1f83d7d1e8c1e05673168c411c32e
BatyaIsHere <rshchibrik@gmail.com>
2019-03-03 00:39:24 +0200

    some fixes

--- main/javascript/Runner.js
+++ main/javascript/Runner.js
@@ -41,12 +41,12 @@ const bulletDirections = (bullets, bulletsOnMap, board) => {
     return bulletsOnMap;
 }
 
-function findDirection(p1, p2){
+function findDirection(p1, p2) {
     let x = p1.x -
         p2.x;
     let y = p1.y - p2.y;
     let str = x + " " + y;
-    switch(str){
+    switch (str) {
         case '1 0':
             str = 'LEFT';
             break;
@@ -76,17 +76,19 @@ function findClosestTank(board) {
         map[x.x][x.y] = 1;
         return map;
     }
+
     return board.getEnemies().map(x => ({
         tank: x,
         path: x.fastestPath(board.getMe(),
             convertDengerBoardToMap(dangerMap, x))
     })).sort((first, second) => {
-        if(first.path===null && second.path === null) return first;
-        if(first.path === null) return 10000 - second.path.length;
-        if(second.path === null) return  -10000 + first.path.length;
+        if (first.path === null && second.path === null) return first;
+        if (first.path === null) return 10000 - second.path.length;
+        if (second.path === null) return -10000 + first.path.length;
         return first.path.length - second.path.length;
     })[0];
 }
+
 const dangerCells = (bulletsArray, board) => {
     const bulletsFly = bulletsArray.reduce((ar, x) => {
         let x1 = x.x, x2 = x.x, y1 = x.y, y2 = x.y;
@@ -112,34 +114,34 @@ const dangerCells = (bulletsArray, board) => {
                 bulletUndef = true;
         }
         if (!bulletUndef) {
-            ar.push({x:x.x, y:x.y});
-            ar.push({ x: x1, y: y1 });
-            ar.push({ x: x2, y: y2 });
-        }else{
-            ar.push({x:x.x, y:x.y});
-            ar.push({ x: x+1, y: x.y });
-            ar.push({ x: x+2, y: x.y });
-            ar.push({ x: x-1, y: x.y });
-            ar.push({ x: x-2, y: x.y });
-            ar.push({ x: x, y: x.y -1});
-            ar.push({ x: x, y: x.y - 2});
-            ar.push({ x: x, y: x.y +1});
-            ar.push({ x: x, y: x.y +2});
+            ar.push({x: x.x, y: x.y});
+            ar.push({x: x1, y: y1});
+            ar.push({x: x2, y: y2});
+        } else {
+            ar.push({x: x.x, y: x.y});
+            ar.push({x: x + 1, y: x.y});
+            ar.push({x: x + 2, y: x.y});
+            ar.push({x: x - 1, y: x.y});
+            ar.push({x: x - 2, y: x.y});
+            ar.push({x: x, y: x.y - 1});
+            ar.push({x: x, y: x.y - 2});
+            ar.push({x: x, y: x.y + 1});
+            ar.push({x: x, y: x.y + 2});
         }
 
         return ar;
     }, []);
     let dangerMap = new Array(board.size())
         .fill([])
-        .map(()=>new Array(board.size()))
-        .map(c=>c.fill(0));
+        .map(() => new Array(board.size()))
+        .map(c => c.fill(0));
     bulletsFly && bulletsFly
-        .filter(x=>x.x >= 0 && x.x < board.size() && x.y >= 0 && x.y < board.size())
-        .forEach(x=>{
+        .filter(x => x.x >= 0 && x.x < board.size() && x.y >= 0 && x.y < board.size())
+        .forEach(x => {
             dangerMap[x.x][x.y] = Elements.BULLET;
         });
-    board.getEnemies().forEach(e=>dangerMap[e.x][e.y] = Elements.TANK_DOWN);
-    board.getBarriers().forEach(e=>dangerMap[e.x][e.y] = Elements.BATTLE_WALL);
+    board.getEnemies().forEach(e => dangerMap[e.x][e.y] = Elements.TANK_DOWN);
+    board.getBarriers().forEach(e => dangerMap[e.x][e.y] = Elements.BATTLE_WALL);
     return dangerMap;
 }
 
@@ -334,26 +336,27 @@ Direction.valueOf = function (index) {
     }
     return Direction.STOP;
 };
+
 //__--DEFAULT CODE END--__
 
-function waveAlgorithm(Map, dest, pos){
+function waveAlgorithm(Map, dest, pos) {
 
-    function getNearbyCells(p,validCB){
+    function getNearbyCells(p, validCB) {
 
-        return [p.shift(-1,0), p.shift(1,0), p.shift(0, -1), p.shift(0,1)]
-            .filter(x=>validCB(x));
+        return [p.shift(-1, 0), p.shift(1, 0), p.shift(0, -1), p.shift(0, 1)]
+            .filter(x => validCB(x));
     }
 
-    function incrementNeighbors(mapSheme, map, wave, pos){
+    function incrementNeighbors(mapSheme, map, wave, pos) {
         //icnrement all nearby to wave cells
         let nextWave = [];
-        if(!wave || !wave.length)return;
+        if (!wave || !wave.length) return;
         let value = mapSheme[wave[0].x][wave[0].y] + 1;
-        wave.forEach(waveI=>{
+        wave.forEach(waveI => {
             getNearbyCells(
-                waveI, (x)=>
+                waveI, (x) =>
                     x.insideSquare(map.length) && !map[x.x][x.y] && !mapSheme[x.x][x.y] && !x.equal(pos)
-            ).forEach(n=>{
+            ).forEach(n => {
                 mapSheme[n.x][n.y] = value;
                 nextWave.push(n);
             })
@@ -363,38 +366,39 @@ function waveAlgorithm(Map, dest, pos){
 
     function decrementedCell(mapSheme, lastStep) {
 
-        return getNearbyCells(lastStep, (x)=>x.insideSquare(mapSheme.length))
-            .find((cell)=>mapSheme[cell.x][cell.y] == mapSheme[lastStep.x][lastStep.y] - 1)
+        return getNearbyCells(lastStep, (x) => x.insideSquare(mapSheme.length))
+            .find((cell) => mapSheme[cell.x][cell.y] == mapSheme[lastStep.x][lastStep.y] - 1)
     }
-    const map = Map.map(row=>row.map(x=>x === 1 ? 0 : 1));
+
+    const map = Map.map(row => row.map(x => x === 1 ? 0 : 1));
     // 0 - if passable
-    pos = new Point(pos.x,pos.y);
-    if(pos.distanceSq(dest) < 2)
+    pos = new Point(pos.x, pos.y);
+    if (pos.distanceSq(dest) < 2)
         return [dest];
 
     let mapSheme = new Array(map.length)
         .fill([])
-        .map(()=>new Array(map.length))
-        .map(c=>c.fill(0))
-        .map((row,x)=>row
-            .map((c,y)=>map[x][y] ? -1 : 0))
+        .map(() => new Array(map.length))
+        .map(c => c.fill(0))
+        .map((row, x) => row
+            .map((c, y) => map[x][y] ? -1 : 0))
     mapSheme[pos.x][pos.y] = 0;
     mapSheme[dest.x][dest.y] = 0;
     //wave its all cells whose neighbors need to be incremented
     let wave = [pos];
     //loop that increments all cells until we come to pos from dest
     let counter = 0;
-    while(!mapSheme[dest.x][dest.y] && counter < 200){
+    while (!mapSheme[dest.x][dest.y] && counter < 200) {
         wave = incrementNeighbors(mapSheme, map, wave, pos);
         counter++;
     }
     //return waveAlgorithm(map, pos, dest);
     let lastStep = dest;
     let path = [];
-    while(!lastStep.equal(pos)){
+    while (!lastStep.equal(pos)) {
         path.unshift(lastStep);
         lastStep = decrementedCell(mapSheme, lastStep);
-        if(!lastStep)return null;
+        if (!lastStep) return null;
     }
     return path;
 }
@@ -425,13 +429,14 @@ class Point {
         return this.y;
     }
 
-    shift(dx,dy){
-        return new Point(this.x+dx, this.y+dy);
+    shift(dx, dy) {
+        return new Point(this.x + dx, this.y + dy);
     }
 
-    insideSquare(size){
+    insideSquare(size) {
         return this.x >= 0 && this.y >= 0 && this.x < size && this.y < size;
     }
+
     moveTo(direction) {
         return pt(direction.changeX(this.x), direction.changeY(this.y));
     }
@@ -440,6 +445,7 @@ class Point {
         //distance from a to b
         return Math.pow(p.x - this.x, 2) + Math.pow(p.y - this.y, 2);
     }
+
     fastestPath(p, map) {
         return waveAlgorithm(map, this, p);
     }
@@ -449,10 +455,10 @@ const getSaveCells = function (x, y, dangerMap) {
     const result = [];
     for (let dx = -1; dx <= 1; dx++) {
         for (let dy = -1; dy <= 1; dy++) {
-            if(x.x >= 0 && x.x < board.size.x && y >= 0 && y < board.size.y) continue;
-            if(!dangerMap[x+dx][y+dy]){
+            if (x.x >= 0 && x.x < board.size.x && y >= 0 && y < board.size.y) continue;
+            if (!dangerMap[x + dx][y + dy]) {
                 const str = dx + " " + dy
-                switch(str){
+                switch (str) {
                     case '-1 0':
                         result.push('LEFT');
                         break;
@@ -472,6 +478,7 @@ const getSaveCells = function (x, y, dangerMap) {
             }
         }
     }
+
     function compareRandom(a, b) {
         return Math.random() - 0.5;
     }
@@ -683,7 +690,9 @@ var Board = function (board) {
     const isNear = function (x, y, element) {
         return getNear(x, y).includes(element);
     };
+    const closestFreeSpace = function (me) {
 
+    };
     const isBarrierAt = function (x, y) {
         if (pt(x, y).isOutOf(size)) {
             return true;
@@ -723,7 +732,7 @@ var Board = function (board) {
 //__--DEFAULT CODE END--__
 
 function getTanksTrajectories(danger, board) {
-    let {tank,path} = findClosestTank(board);
+    let {tank, path} = findClosestTank(board);
     let tempDanger = danger.map(row => [...row]);
     for (let x = tank.x + 1; x < 43; x++) {
         if (danger[x][tank.y] !== Elements.BATTLE_WALL) {
@@ -746,12 +755,12 @@ function getTanksTrajectories(danger, board) {
         } else break;
     }
     let temp = tempDanger
-        .reduce((ar,row, x) => {
-            ar.push(...row.map((cell, y) => ({ coords: new Point(x, y), value: cell})))
+        .reduce((ar, row, x) => {
+            ar.push(...row.map((cell, y) => ({coords: new Point(x, y), value: cell})))
             return ar;
-        },[]).filter(o => typeof o.value == "number" && o.value > 0);
+        }, []).filter(o => typeof o.value == "number" && o.value > 0);
     let str = "";
-    if(temp.find(x=>x.coords.equal(board.getMe()))){
+    if (temp.find(x => x.coords.equal(board.getMe()))) {
         str = ",ACT";
     }
     let nextPoint;
@@ -760,25 +769,35 @@ function getTanksTrajectories(danger, board) {
     //     if(i.coords.x == path[0].x+1 && currentStep - lastShot >= 3)nextPoint.x++;
     //     else  if(i.coords.x == path[0].x-1 && currentStep - lastShot >= 3)nextPoint.x--;
     // })
-    if(currentStep - lastShot <= path[0].length-1 || canShot())
-    return findDirection(board.getMe(), nextPoint) + str;
+    // if (path.length < 2 && currentStep - lastShot <= 3)
+    //     return "STOP"
+    if (currentStep - lastShot <= path[0].length - 1 || canShot())
+        return findDirection(board.getMe(), nextPoint) + str;
+
     return findDirection(board.getMe(), nextPoint);
 }
+
 const has = (array, point) => {
     return array.some(x => x.x == point.x && x.y == point.y);
 }
-const fireToDangerDirect = (tank)=>{
+const fireToDangerDirect = (tank) => {
     let {x, y} = tank;
     bulletsOnMap
-        .filter(x=>x.direction)
-        .filter(b=>b.x == x || b. y == y)
-        .sort((a,b)=> Math.abs(x-a.x) + Math.abs(y-a.y) - Math.abs(y-b.y) + Math.abs(x-a.x));
+        .filter(x => x.direction)
+        .filter(b => b.x == x || b.y == y)
+        .sort((a, b) => Math.abs(x - a.x) + Math.abs(y - a.y) - Math.abs(y - b.y) + Math.abs(x - a.x));
     return bulletsOnMap && bulletsOnMap.length && bulletsOnMap[0].direction + ",ACT";
 }
-function canShot(){
+
+function canShot() {
     return currentStep - lastShot >= 4;
 }
 
+// function neededEscape(tank,board){
+//     if(this.fastestPath(tank,board).length<=1 && !canShot()){
+// if(board.getMe())
+//     }
+// }
 function bulletCreator() {
     class Bullet extends Point {
         constructor(x, y, id) {
@@ -818,6 +837,7 @@ function bulletCreator() {
     }
     return {create: this.createBullet};
 }
+
 let currentStep = 0;
 let lastShot = 0;
 let readyToFire = true;
@@ -829,24 +849,24 @@ const DirectionSolver = function (board) {
     return {
         get: function () {
             currentStep++;
-            console.log("current step",currentStep)
-            console.log("last shot",lastShot)
+            console.log("current step", currentStep)
+            console.log("last shot", lastShot)
             const tank = board.getMe();
             if (!tank)
                 return "STOP"
             bulletsOnMap = bulletDirections(board.getBullets(), bulletsOnMap, board);
             dangerMap = dangerCells(bulletsOnMap, board);
-            let res1= getTanksTrajectories(dangerMap, board)
-            if(res1.indexOf('ACT') + 1) lastShot=currentStep;
+            let res1 = getTanksTrajectories(dangerMap, board)
+            if (res1.indexOf('ACT') + 1) lastShot = currentStep;
             return res1;
 
             let freeSpaceToMove = getSaveCells(tank.x, tank.y, dangerMap);
-            if(freeSpaceToMove.length){
+            if (freeSpaceToMove.length) {
 
                 return freeSpaceToMove[0] + ",ACT";
-            }else{
+            } else {
                 let res = fireToDangerDirect(tank);
-                if(res.indexOf('ACT') + 1) lastShot=currentStep;
+                if (res.indexOf('ACT') + 1) lastShot = currentStep;
                 return res;
             }
 
